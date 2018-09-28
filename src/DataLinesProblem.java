6fd0e2cb6f4d710d63d3602f2ddd0bf38145f908
heroys6 <email@example.com>
2018-09-28 19:21:41 +0300

    Code readability is improved

--- src/DataLinesProblem.java
+++ src/DataLinesProblem.java
@@ -1,13 +1,14 @@
 import domain.DataLine;
-import javafx.util.Pair;
 import validator.DataLineValidator;
 
+import javafx.util.Pair;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,7 +23,7 @@ public class DataLinesProblem {
      * @param args application arguments
      */
     public static void main(String[] args) {
-        List<DataLine> buffer = new ArrayList<>();
+        List<DataLine> buffer = new ArrayList<>(); // data lines(C) accumulator
 
         try (Stream<String> fileStream = Files.lines(Paths.get("resources/input.txt"))) {
             fileStream
@@ -36,60 +37,64 @@ public class DataLinesProblem {
                                     l.add(new Pair<>(e, new ArrayList<>(buffer))); // copy current buffer state
                             }, ArrayList::addAll)
                     .stream()
-                    .map(pair -> { // process stream of (query, lines) and transform to the stream of results
-                        DataLine query = pair.getKey();
-                        List<DataLine> dLines = pair.getValue();
-
-                        List<DataLine> filteredLines = dLines.stream() // filter out dLines
-                                .filter(dLine -> { // service filter
-                                    if (query.getServiceNumber() == 0)
-                                        return true;
-                                    else if (query.getServiceVariation() == 0)
-                                        return query.getServiceNumber().equals(dLine.getServiceNumber());
-                                    else
-                                        return query.getServiceVariation().equals(dLine.getServiceVariation());
-                                })
-                                .filter(dLine -> { // question filter
-                                    if (query.getQuestionType() == 0) // 0 == * in query
-                                        return true;
-                                    else if (query.getQuestionType().equals(dLine.getQuestionType())) {
-
-                                        if (query.getQuestionCategory() == 0) // 0 == * in query
-                                            return true;
-                                        else if (query.getQuestionCategory().equals(dLine.getQuestionCategory())) {
-
-                                            if (query.getQuestionSubCategory() == 0) // 0 == * in query
-                                                return true;
-                                            else return query.getQuestionSubCategory().equals(
-                                                    dLine.getQuestionSubCategory());
-                                        } else
-                                            return false;
-                                    } else
-                                        return false;
-                                })
-                                .filter(dLine -> { // date filter
-                                    LocalDate startDate = query.getResponseDate();
-                                    LocalDate endDate = query.getResponseDate2();
-
-                                    if (startDate.isEqual(endDate)) // endDate is omitted or equals
-                                        return dLine.getResponseDate().isEqual(startDate);
-
-                                    // Rule: dLine.date >= query.date1 && dLine.date <= query.date2
-                                    return !(dLine.getResponseDate().isBefore(startDate) ||
-                                            dLine.getResponseDate().isAfter(endDate));
-                                })
-                                .collect(Collectors.toList());
-
-                        int timeSum = filteredLines.stream()
-                                .map(DataLine::getTime)
-                                .reduce(0, (acc, n) -> acc + n);
-                        int filteredQuantity = filteredLines.size();
-
-                        return timeSum == 0 ? "-" : String.valueOf(timeSum / filteredQuantity); // avgTime as result
-                    })
+                    .map(pair2result)
                     .forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    // Transform stream of (query, lines) to the stream of results
+    private static Function<Pair<DataLine, List<DataLine>>, String> pair2result = pair -> {
+        DataLine query = pair.getKey();
+        List<DataLine> dLines = pair.getValue();
+
+        // Apply filters from the query to dLines
+        List<DataLine> filteredLines = dLines.stream()
+                .filter(dLine -> { // service filter
+                    if (query.getServiceNumber() == 0)
+                        return true;
+                    else if (query.getServiceVariation() == 0)
+                        return query.getServiceNumber().equals(dLine.getServiceNumber());
+                    else
+                        return query.getServiceVariation().equals(dLine.getServiceVariation());
+                })
+                .filter(dLine -> { // question filter
+                    if (query.getQuestionType() == 0) // 0 == * in query
+                        return true;
+                    else if (query.getQuestionType().equals(dLine.getQuestionType())) {
+
+                        if (query.getQuestionCategory() == 0) // 0 == * in query
+                            return true;
+                        else if (query.getQuestionCategory().equals(dLine.getQuestionCategory())) {
+
+                            if (query.getQuestionSubCategory() == 0) // 0 == * in query
+                                return true;
+                            else return query.getQuestionSubCategory().equals(
+                                    dLine.getQuestionSubCategory());
+                        } else
+                            return false;
+                    } else
+                        return false;
+                })
+                .filter(dLine -> { // date filter
+                    LocalDate startDate = query.getResponseDate();
+                    LocalDate endDate = query.getResponseDate2();
+
+                    if (startDate.isEqual(endDate)) // endDate is omitted or equals
+                        return dLine.getResponseDate().isEqual(startDate);
+
+                    // Rule: dLine.date >= query.date1 && dLine.date <= query.date2
+                    return !(dLine.getResponseDate().isBefore(startDate) ||
+                            dLine.getResponseDate().isAfter(endDate));
+                })
+                .collect(Collectors.toList());
+
+        int timeSum = filteredLines.stream()
+                .map(DataLine::getTime)
+                .reduce(0, (acc, n) -> acc + n);
+        int filteredQuantity = filteredLines.size();
+
+        return timeSum == 0 ? "-" : String.valueOf(timeSum / filteredQuantity); // avgTime as result
+    };
 }
